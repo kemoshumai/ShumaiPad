d7cba7267c6e11506e0f9c1c575b97d6c548c618
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2022-01-27 01:28:27 +0900

    Add debug log

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const RPCHandle = require('./src/RPCHandle');
 
 const main = async () => {
 
+    console.log("Lauching...");
     const app = await carlo.launch({
         width:400,
-        height:880
+        height:1000
     });
-    app.on('exit', () => process.exit());
+    app.on('exit', () =>{
+        console.log("carlo exit");
+        process.exit();
+    });
     app.serveFolder(__dirname);
     await app.exposeFunction('env', _ => process.env);
     await app.load("index.html",rpc.handle(new RPCHandle));
