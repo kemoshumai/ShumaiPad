83e00175a9b213b9808f40a6d4e8288937f45a7a
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2022-07-06 19:42:05 +0900

    Rewrite to v0.2.0

--- src/about.js
+++ src/about.js
@@ -2,7 +2,7 @@ const about = () => {
     Swal.fire({
         imageUrl: '/res/KemoshumaiLogoLit.png',
         title:'ShumaiPad',
-        text : `v0.1.2
+        text : `v0.2.0
         Made by Kemoshumai.`
     });
 }
