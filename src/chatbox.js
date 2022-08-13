106e7dab646da34563e658f7804b69b6c87ff8a9
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2022-08-13 19:14:13 +0900

    Add cooltime to send to chatbox

--- src/chatbox.js
+++ src/chatbox.js
@@ -1,14 +1,18 @@
 const osc = require('node-osc');
 const oscclient = new osc.Client('127.0.0.1', 9000);
 
+let latestUpdateTime = 0;
+
 const sendMessageToVRC_Chatbox = (text) => {
     oscclient.send('/chatbox/typing', false);
     oscclient.send('/chatbox/input', text, true);
 }
 
 const setProposalMessageToVRC_Chatbox = (lasttext) => {
+    if(Date.now() - latestUpdateTime < 1500) return;// スパム判定対策
     oscclient.send('/chatbox/typing', true);
     oscclient.send('/chatbox/input', lasttext, true);
+    latestUpdateTime = Date.now();
 }
 
 module.exports.sendMessageToVRC_Chatbox = sendMessageToVRC_Chatbox;
