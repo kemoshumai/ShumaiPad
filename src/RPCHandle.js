b96dfa3124302de6ba4d30e1dbc657741bd6eeca
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2022-08-13 19:14:42 +0900

    Add chatbox api to RPCHandle

--- src/RPCHandle.js
+++ src/RPCHandle.js
@@ -1,12 +1,16 @@
 const { sendMessageToVRC_Clipboard,setProposalMessageToVRC_Clipboard } = require("./sendToVRC");
+const { sendMessageToVRC_Chatbox, setProposalMessageToVRC_Chatbox } = require('./chatbox');
 const { startRecognizeWithVosk, stopRecognizeWithVosk } = require("./vosk");
+const { japaneseToRomaji } = require("./languageanalysis");
 
 module.exports = class RPCHandle{
     sendMessageToVRC(text,subtext){
         sendMessageToVRC_Clipboard(text,subtext);
+        sendMessageToVRC_Chatbox(japaneseToRomaji(text));
     }
     setProposalMessageToVRC(lasttext){
         setProposalMessageToVRC_Clipboard(lasttext);
+        setProposalMessageToVRC_Chatbox(japaneseToRomaji(lasttext));
     }
     debugLog(...text){
         console.log(...text);
